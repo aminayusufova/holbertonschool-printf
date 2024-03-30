af86d103c36b6117d25d6811a762fd60392257b5
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-03-30 22:54:07 +0400

    _printf.c

--- _printf.c
+++ _printf.c
@@ -10,56 +10,61 @@
  */
 int _printf(const char *format, ...)
 {
-    int i, j, count;
-    va_list list;
-    set arguments[] = {
-        {'c', print_char},
-        {'d', print_d},
-        {'i', print_d},
-        {'s', print_str},
-    };
+	int i, j, count, find;
+	va_list list;
+	set arguments[] = {
+		{'c', print_char},
+		{'d', print_d},
+		{'i', print_d},
+		{'s', print_str},
+	};
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(list, format);
-    count = 0;
+	va_start(list, format);
 
-    for (i = 0; format[i]; i++)
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == '\0')
-                return count;
+	count = 0;
 
-            for (j = 0; j < 4; j++)
-            {
-                if (format[i + 1] == arguments[j].spec)
-                {
-                    count += arguments[j].print(list);
-                    i++;
-                    break;
-                }
-            }
+	for (i = 0; *(format + i); i++)
+	{
+		if (*(format + i) == '%')
+		{
+			if (*(format + i + 1) == '\0')
+				continue;
+			find = 0;
 
-            if (j == 4)
-            {
-                if (format[i + 1] == '%')
-                {
-                    count += write(1, "%", 1);
-                    i++;
-                }
-                else
-                {
+			for (j = 0; j < 4; j++)
+			{
+				if (*(format + i + 1) == arguments[j].spec)
+				{
+					count += arguments[j].print(list);
+					find = 1;
+					format++;
+					break;
+				}
+			}
+
+			if (find != 1)
+			{
+				if (*(format + i + 1) == '%')
+				{
+					count += write(1, "%", 1);
+					format++;
+				}
+				else {
                     count += write(1, &format[i], 1);
+                    count += write(1, &format[i + 1], 1); 
+                    i++;
                 }
-            }
-        }
-        else
-        {
-            count += write(1, &format[i], 1);
-        }
-    }
-    va_end(list);
-    return (count);
-}
+
+		}
+		else
+		{
+			count += write(1, (format + i), 1);
+		}
+	}
+	va_end(list);
+
+	return (count);
+} ищи ошибку тут
