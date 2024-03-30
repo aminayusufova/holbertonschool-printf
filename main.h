18be56c61d7adf062e7d8d078d718ac8f01ec676
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-03-30 15:41:03 +0400

    main.h

--- main.h
+++ main.h
@@ -1,1 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct Set - a specifier - function pair
+ * @spec: Char - the format specifier
+ * @print: Special function implemented to print data of a given
+ * specifier to stdout
+ *
+ * Description:  specifier - function pair
+ */
+typedef struct Set
+{
+	char spec;
+	int (*print)(va_list arg);
+} set;
+
+int print_d(va_list list);
+int print_str(va_list list);
+int print_char(va_list list);
+int _printf(const char*, ...);
+
+#endif
