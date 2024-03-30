49c8ba1eaeb3a48d77af8869b09688ac157ccd1b
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-03-30 16:30:39 +0400

    Update print_str.c

--- print_str.c
+++ print_str.c
@@ -9,20 +9,21 @@
 int print_str(va_list list)
 {
 	int count;
-	char *s;
+	
 
-	s = va_arg(list, char*);
+	char *s = va_arg(list, char*)
 
-	if (*s == '\0')
-		return (0);
-
+	if (s == NULL)
+   	 {
+        	s = "(null)";
+    	}
 	count = 0;
+    	while (*s)
+    	{
+        count += write(1, s, 1);
+        s++;
+    	}
 
-	while (*s)
-	{
-		count += write(1, s, 1);
-		s++;
-	}
 
 	return (count);
 }
